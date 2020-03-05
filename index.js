38d717287dafef0d2ae6d53792b09a895ab75893
Alok Kumar Mishra <alok@Aloks-MacBook-Pro.local>
2020-03-05 16:40:58 +0530

    activity monitor added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './routernavi';
+import App from './src/CustomActivityMonitor/customNavigation';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/CustomActivityMonitor/customNavigation.js
@@ -0,0 +1,56 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text, Image, SafeAreaView} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ' ',
+      input: ' ',
+      viewOpacity: 0,
+    };
+  }
+  onChangeText() {}
+
+  render() {
+    const {navigation} = this.props;
+
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+          }}>
+          <TouchableOpacity onPress={() => this.setState({viewOpacity: 1})}>
+            <Text style={{fontSize: 40, color: 'green'}}>click to start</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            opacity: this.state.viewOpacity,
+          }}>
+          <Image
+            style={{alignSelf: 'center', width: '50%'}}
+            source={require('../CustomActivityMonitor/loader.gif')}
+          />
+        </View>
+        <View style={{flex: 1, backgroundColor: '#fff', alignItems: 'center'}}>
+          <TouchableOpacity onPress={() => this.setState({viewOpacity: 0})}>
+            <Text style={{fontSize: 40, color: 'red'}}>click to stop</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+export default Home;
