99df687f2d732803bb2085f967a28191f613d94c
Alok Kumar Mishra <alok@Aloks-MacBook-Pro.local>
2020-03-13 09:44:23 +0530

    sprint 3

--- src/components/SectionList.js
+++ src/components/SectionList.js
@@ -15,28 +15,88 @@ class Section extends React.Component {
       itemsArray: [
         {
           categoryId: 'fruits',
-          title: 'seasonal fruits',
+          title: 'mango',
           quantity: 2,
         },
         {
           categoryId: 'fruits',
-          title: 'regional fruits',
+          title: 'apple',
+          quantity: 5,
+        },
+        {
+          categoryId: 'fruits',
+          title: 'coconut',
+          quantity: 4,
+        },
+        {
+          categoryId: 'fruits',
+          title: 'orange',
+          quantity: 2,
+        },
+        {
+          categoryId: 'fruits',
+          title: 'pomegranade',
+          quantity: 2,
+        },
+        {
+          categoryId: 'fruits',
+          title: 'mausmi',
           quantity: 3,
         },
         {
           categoryId: 'flowers',
-          title: 'seasonal fruits',
+          title: 'rose',
           quantity: 1,
         },
         {
           categoryId: 'flowers',
-          title: 'yearly fruits',
+          title: 'lili',
           quantity: 4,
         },
         {
+          categoryId: 'flowers',
+          title: 'jasmine',
+          quantity: 2,
+        },
+        {
+          categoryId: 'flowers',
+          title: 'hibiscus',
+          quantity: 8,
+        },
+        {
+          categoryId: 'flowers',
+          title: 'daffodils',
+          quantity: 9,
+        },
+        {
+          categoryId: 'flowers',
+          title: 'seasonal flowers',
+          quantity: 1,
+        },
+        {
+          categoryId: 'flowers',
+          title: 'sregional fruits',
+          quantity: 1,
+        },
+        {
           categoryId: 'vegetables',
-          title: 'seasonal veg',
-          quantity: 3,
+          title: 'potato',
+          quantity: 8,
+        },
+        {
+          categoryId: 'vegetables',
+          title: 'tomato',
+          quantity: 5,
+        },
+        {
+          categoryId: 'vegetables',
+          title: 'guard',
+          quantity: 2,
+        },
+        {
+          categoryId: 'vegetables',
+          title: 'brinjal',
+          quantity: 6,
         },
       ],
     };
